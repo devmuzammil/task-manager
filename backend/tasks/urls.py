ae6c1619f00d5714cab90b5f8aad836d0ea01f9b
devmuzammil <muzammilhussainabro@gmail.com>
2026-08-11 12:48:32 +0500

    Completed the CrudAPI

---
+++ backend/tasks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import TaskListView, TaskDetailView
+
+
+urlpatterns = [
+    path("tasks/", TaskListView.as_view()),
+    path("tasks/<int:pk>/", TaskDetailView.as_view()),
+]
